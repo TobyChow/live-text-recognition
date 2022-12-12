552511cdb50b33740fd15f24af49cb64350935f1
Toby Chow <toby_chow@hotmail.com>
2022-12-12 00:08:10 -0500

    initial

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useMemo} from 'react';
 import {
   Camera,
   useCameraDevices,
   useFrameProcessor,
 } from 'react-native-vision-camera';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Animated, SafeAreaView, LogBox} from 'react-native';
 import {scanOCR} from 'vision-camera-ocr';
 import {
   useSharedValue,
   useAnimatedStyle,
   runOnJS,
-  Animated,
 } from 'react-native-reanimated';
 
 export default function App() {
@@ -20,8 +19,28 @@ export default function App() {
   const devices = useCameraDevices();
   const device = devices.back;
 
+  const format = useMemo(() => {
+    const desiredWidth = 1280;
+    const desiredHeight = 720;
+    if (device) {
+      for (let index = 0; index < device.formats.length; index++) {
+        const format = device.formats[index];
+        if (format) {
+          console.log("h: "+format.videoHeight);
+          console.log("w: "+format.videoWidth);
+          if (format.videoWidth == desiredWidth && format.videoHeight == desiredHeight){
+            console.log("select format: "+format);
+            console.log(JSON.stringify(format));
+            return format;
+          }
+        }
+      };
+    }
+    return undefined;
+  }, [device?.formats]);
+
   // represents position of the cat on the screen 🐈
-  const catBounds = useSharedValue({top: 0, left: 0, right: 0, bottom: 0});
+  const catBounds = useSharedValue({top: 0, left: 0, right: 100, bottom: 100});
 
   useEffect(() => {
     (async () => {
@@ -32,9 +51,10 @@ export default function App() {
 
   const frameProcessor = useFrameProcessor(frame => {
     'worklet';
+    console.log(frame.width, frame.height);
     const scannedOcr = scanOCR(frame);
     if (scannedOcr) {
-      console.log(JSON.stringify(scannedOcr.result));
+      //console.log(JSON.stringify(scannedOcr.result));
     }
     if (scannedOcr.result?.text) {
       runOnJS(setScannedOcrResult)(scannedOcr.result.text);
@@ -43,15 +63,15 @@ export default function App() {
 
   // uses 'catBounds' to position the red rectangle on screen.
   // smoothly updates on UuseAnimatedStyle(() => ({
-  const boxOverlayStyle = useAnimatedStyle(
-    () => ({
-      position: 'absolute',
-      borderWidth: 1,
-      borderColor: 'red',
-      ...catBounds.value,
-    }),
-    [catBounds],
-  );
+  const boxOverlayStyle = {
+    position:'absolute',
+    top:685,
+    left:0,
+    width:322,
+    height:90,
+    borderWidth:3,
+    borderColor:'red',
+  };
 
   return (
     device != null &&
@@ -61,9 +81,11 @@ export default function App() {
           style={StyleSheet.absoluteFill}
           device={device}
           isActive={false}
-          frameProcessor={frameProcessor}
+          // frameProcessor={frameProcessor}
         />
-        <Animated.View style={boxOverlayStyle} />
+        <View style={boxOverlayStyle}>
+          <Text>BOX</Text>
+        </View>
         <View>
           <Text>{scannedOcrResult}</Text>
         </View>
