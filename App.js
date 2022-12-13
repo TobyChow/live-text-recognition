5e71c8628159a8c624df632463c01ff3602cf2d3
Toby Chow <toby_chow@hotmail.com>
2022-12-13 18:12:34 -0500

    working sample

--- App.js
+++ App.js
@@ -1,95 +1,145 @@
-import {useState, useEffect, useMemo} from 'react';
+/* eslint-disable prettier/prettier */
+import { useState, useEffect, useMemo } from 'react';
 import {
-  Camera,
-  useCameraDevices,
-  useFrameProcessor,
+    Camera,
+    useCameraDevices,
+    useFrameProcessor,
 } from 'react-native-vision-camera';
-import {View, Text, StyleSheet, Animated, SafeAreaView, LogBox} from 'react-native';
-import {scanOCR} from 'vision-camera-ocr';
+import { View, Text, StyleSheet, Animated, SafeAreaView, LogBox, Dimensions } from 'react-native';
+import { scanOCR } from 'vision-camera-ocr';
 import {
-  useSharedValue,
-  useAnimatedStyle,
-  runOnJS,
+    useSharedValue,
+    useAnimatedStyle,
+    runOnJS,
 } from 'react-native-reanimated';
 
 export default function App() {
-  const [hasPermission, setHasPermission] = useState(false);
-  const [scannedOcrResult, setScannedOcrResult] = useState();
-  
-  const devices = useCameraDevices();
-  const device = devices.back;
+    const [hasPermission, setHasPermission] = useState(false);
+    const [scannedOcrResult, setScannedOcrResult] = useState();
+    const [matchedFrame, setMatchedFrame] = useState();
 
-  const format = useMemo(() => {
-    const desiredWidth = 1280;
-    const desiredHeight = 720;
-    if (device) {
-      for (let index = 0; index < device.formats.length; index++) {
-        const format = device.formats[index];
-        if (format) {
-          console.log("h: "+format.videoHeight);
-          console.log("w: "+format.videoWidth);
-          if (format.videoWidth == desiredWidth && format.videoHeight == desiredHeight){
-            console.log("select format: "+format);
-            console.log(JSON.stringify(format));
-            return format;
-          }
+    const devices = useCameraDevices();
+    const device = devices.back;
+
+    // represents position of the cat on the screen 🐈
+    const catBounds = useSharedValue({ top: 0, left: 0, right: 100, bottom: 100 });
+
+    useEffect(() => {
+        (async () => {
+            const status = await Camera.requestCameraPermission();
+            setHasPermission(status === 'authorized');
+        })();
+    }, []);
+
+    const frameProcessor = useFrameProcessor(frame => {
+        'worklet';
+        const scannedOcr = scanOCR(frame);
+        if (scannedOcr) {
+            if (scannedOcr.result.text === 'adidas') {
+                const matchedFrame = scannedOcr.result.blocks[0].frame;
+                console.log(matchedFrame);
+                runOnJS(setMatchedFrame)(matchedFrame);
+            }
         }
-      };
+        if (scannedOcr.result?.text) {
+            runOnJS(setScannedOcrResult)(scannedOcr.result.text);
+        }
+    }, []);
+
+    const format = useMemo(() => {
+        const desiredWidth = 1280;
+        const desiredHeight = 720;
+        if (device) {
+          for (let index = 0; index < device.formats.length; index++) {
+            const format = device.formats[index];
+            if (format) {
+              console.log("h: "+format.videoHeight);
+              console.log("w: "+format.videoWidth);
+              if (format.videoWidth == desiredWidth && format.videoHeight == desiredHeight){
+                console.log("select format: "+ JSON.stringify(format));
+                return format;
+              }
+            }
+          };
+        }
+        return undefined;
+      }, [device?.formats]);
+
+    const { width: screenW, height: screenH } = Dimensions.get('screen');
+
+    const {wRatio, hRatio} = screenToFrameRatio(screenW, screenH, 720, 1280);
+    /*
+    const f = {
+        "boundingCenterY": 683,
+        "x": 384.5,
+        "width": 437,
+        "y": 683,
+        "boundingCenterX": 384,
+        "height": 108
     }
-    return undefined;
-  }, [device?.formats]);
+    */
 
-  // represents position of the cat on the screen 🐈
-  const catBounds = useSharedValue({top: 0, left: 0, right: 100, bottom: 100});
+    // uses 'catBounds' to position the red rectangle on screen.
+    // smoothly updates on UuseAnimatedStyle(() => ({
 
-  useEffect(() => {
-    (async () => {
-      const status = await Camera.requestCameraPermission();
-      setHasPermission(status === 'authorized');
-    })();
-  }, []);
+    let boxOverlayStyle = {
+        position: 'absolute',
+        borderWidth: 3,
+        borderColor: 'red',
+        top:0,
+        left:0,
+        width:50,
+        height:50,
+    };
 
-  const frameProcessor = useFrameProcessor(frame => {
-    'worklet';
-    console.log(frame.width, frame.height);
-    const scannedOcr = scanOCR(frame);
-    if (scannedOcr) {
-      //console.log(JSON.stringify(scannedOcr.result));
+    let matchedOverlayStyle = {};
+    if (matchedFrame) {
+        const topPos = matchedFrame.y * 1 / hRatio;
+        const leftPos = matchedFrame.x * 1 / wRatio;
+        const boxHeight = matchedFrame.height * 1 / hRatio;
+        const boxWidth = matchedFrame.width * 1 / wRatio;
+        matchedOverlayStyle = {
+            top: topPos - (boxHeight * 0.5),
+            left: leftPos - (boxWidth * 0.5),
+            width: boxWidth,
+            height: boxHeight,
+        };
+
+        boxOverlayStyle = Object.assign(boxOverlayStyle, matchedOverlayStyle);
     }
-    if (scannedOcr.result?.text) {
-      runOnJS(setScannedOcrResult)(scannedOcr.result.text);
-    }
-  }, []);
 
-  // uses 'catBounds' to position the red rectangle on screen.
-  // smoothly updates on UuseAnimatedStyle(() => ({
-  const boxOverlayStyle = {
-    position:'absolute',
-    top:685,
-    left:0,
-    width:322,
-    height:90,
-    borderWidth:3,
-    borderColor:'red',
-  };
-
-  return (
-    device != null &&
-    hasPermission && (
-      <>
-        <Camera
-          style={StyleSheet.absoluteFill}
-          device={device}
-          isActive={false}
-          // frameProcessor={frameProcessor}
-        />
-        <View style={boxOverlayStyle}>
-          <Text>BOX</Text>
-        </View>
-        <View>
-          <Text>{scannedOcrResult}</Text>
-        </View>
-      </>
-    )
-  );
+    return (
+        device != null &&
+        hasPermission && (
+            <>
+                <Camera
+                    style={StyleSheet.absoluteFill}
+                    device={device}
+                    isActive={true}
+                    // format={format}
+                    frameProcessor={frameProcessor}
+                    frameProcessorFps={1}
+                />
+                {matchedFrame  &&
+                <View style={boxOverlayStyle}>
+                    <Text>BOX4</Text>
+                </View>
+                }
+                <View>
+                    <Text>{scannedOcrResult}</Text>
+                    <Text>{`dim: ${screenW}, ${screenH}`}</Text>
+                </View>
+            </>
+        )
+    );
 };
+
+
+function screenToFrameRatio(screenW, screenH, frameW, frameH) {
+    return {
+        wRatio: frameW / screenW,
+        hRatio: frameH / screenH,
+        // wRatio: 1,
+        // hRatio: 1,
+    };
+}
---
+++ scratch.js
@@ -0,0 +1,10 @@
+/*
+* npx react-native start
+* 
+*/
+
+// text recogition resposne
+{"blocks":[{"recognizedLanguages":["es"],"text":"adidas","lines":[{"recognizedLanguages":["es"],"text":"adidas","cornerPoints":[{"x":166,"y":635},{"x":601,"y":628},{"x":602,"y":730},{"x":167,"y":737}],"elements":[],"frame":{"boundingCenterY":682,"width":436,"x":384,"y":682.5,"boundingCenterX":384,"height":109}}],"cornerPoints":[{"x":166,"y":635},{"x":601,"y":629},{"x":603,"y":731},{"x":167,"y":737}],"frame":{"boundingCenterY":683,"x":384.5,"width":437,"y":683,"boundingCenterX":384,"height":108}}],"text":"adidas"}
+
+// devices.format
+{"pixelFormat":"420v","videoStabilizationModes":["off","auto","standard"],"maxZoom":8,"minISO":29,"maxISO":941,"autoFocusSystem":"none","supportsPhotoHDR":false,"colorSpaces":["jpeg"],"videoWidth":1280,"videoHeight":720,"photoWidth":1280,"frameRateRanges":[{"maxFrameRate":60,"minFrameRate":1},{"maxFrameRate":15,"minFrameRate":15},{"maxFrameRate":24,"minFrameRate":6},{"maxFrameRate":24,"minFrameRate":24},{"maxFrameRate":30,"minFrameRate":7},{"maxFrameRate":30,"minFrameRate":8},{"maxFrameRate":30,"minFrameRate":30}],"photoHeight":720,"supportsVideoHDR":false,"isHighestPhotoQualitySupported":false,"fieldOfView":70.54384665367084}
